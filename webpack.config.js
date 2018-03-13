85b74c6572cee05034aa570341b73e7b9f70052c
Daiz <taneli.vatanen@gmail.com>
2018-03-13 16:51:58 +0200

    Update webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,43 @@
-const config = require("./webpack.config.dev.js");
-const header = require("./us-header");
-const { BannerPlugin } = require("webpack");
-config.plugins.push(
-  new BannerPlugin({
-    banner: header(true),
-    raw: true,
-    entryOnly: true
-  })
-);
-module.exports = config;
+const { CheckerPlugin } = require("awesome-typescript-loader");
+const { resolve, join, dirname, basename } = require("path");
+const { main } = require("./package.json");
+const dir = dirname(main);
+const file = basename(main);
+
+module.exports = {
+  entry: {
+    main: "./src/index.ts"
+  },
+
+  output: {
+    filename: file,
+    path: join(__dirname, dir),
+    publicPath: "/"
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        exclude: resolve(__dirname, "node_modules"),
+        include: resolve(__dirname, "src")
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+
+  plugins: [new CheckerPlugin()]
+};
