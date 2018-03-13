0f62946a2b28f336a89998cd3f5d4a35609e0ae0
Daiz <taneli.vatanen@gmail.com>
2018-03-13 15:30:47 +0200

    Move current webpack config to dev and make a production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,11 @@
-const { CheckerPlugin } = require("awesome-typescript-loader");
-const { resolve, join, dirname, basename } = require("path");
-const main = process.env.npm_package_main
-const dir = dirname(main);
-const file = basename(main);
-
-module.exports = {
-  entry: {
-    loader: "./src/index.ts"
-  },
-
-  output: {
-    filename: file,
-    path: join(__dirname, dir),
-    publicPath: "/"
-  },
-
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: "awesome-typescript-loader",
-        exclude: resolve(__dirname, "node_modules"),
-        include: resolve(__dirname, "src")
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader"
-      }
-    ]
-  },
-
-  plugins: [new CheckerPlugin()]
-};
+const config = require("./webpack.config.dev.js");
+const header = require("./us-header");
+const { BannerPlugin } = require("webpack");
+config.plugins.push(
+  new BannerPlugin({
+    banner: header(true),
+    raw: true,
+    entryOnly: true
+  })
+);
+module.exports = config;
