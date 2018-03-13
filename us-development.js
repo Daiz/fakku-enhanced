51155c45a0bb9ac5f9e23f6d3cb6cd114129f707
Daiz <taneli.vatanen@gmail.com>
2018-03-13 17:54:55 +0200

    Make development userscript reloading more robust

--- us-development.js
+++ us-development.js
@@ -1,23 +1,54 @@
 module.exports = function(host = "localhost", port = 8080) {
   return `
-const socket = new WebSocket("ws://${host}:${port}");
+const SERVER = "ws://${host}:${port}";
+let socket;
 let reload = false;
+let connected = false;
+let timer = null;
+
+function init() {
+  socket = new WebSocket(SERVER);
+  socket.onopen = (event) => {
+    connected = true;
+    clearInterval(timer);
+    if (reload) {
+      console.log("Reconnected to reload server.");
+      if (!document.hidden) {
+        location.reload();
+      }
+    } else {
+      console.log("Connected to reload server.");
+    }
+  };
+  socket.onmessage = (event) => {
+    if (event.data === "reload") {
+      if (!document.hidden) {
+        location.reload();
+      } else {
+        reload = true;
+      }
+    }
+  };
+  socket.onclose = close;
+}
+
+function reconnect() {
+  console.log("Attempting to reconnect...");
+  init();
+}
+
+function close() {
+  connected = false;
+  console.error("Disconnected from reload server.");
+  timer = setInterval(reconnect, 2500);
+}
+
 document.addEventListener("visibilitychange", () => {
-  if (!document.hidden && reload) {
+  if (!document.hidden && connected && reload) {
     location.reload();
   }
 });
-socket.onopen = (event) => {
-  console.log("Connected to reload server.");
-};
-socket.onmessage = (event) => {
-  if (event.data === "reload") {
-    if (!document.hidden) {
-      location.reload();
-    } else {
-      reload = true;
-    }
-  }
-};
+
+init();
 `;
 };
