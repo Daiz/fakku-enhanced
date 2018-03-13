14ca4d30844b4bd1e983f02a857cc696d4d6da60
Daiz <taneli.vatanen@gmail.com>
2018-03-13 16:52:16 +0200

    Build in release script

--- release.js
+++ release.js
@@ -1,17 +1,42 @@
 require("shelljs/global");
 const semver = require("semver");
+const webpack = require("webpack");
 const pretty = require("prettier-package-json");
 const pkg = require("./package.json");
-const oldVersion = pkg.version;
+const header = require("./us-header");
+const config = require("./webpack.config.js");
+const { promisify } = require("util");
+const { resolve } = require("path");
+const path = resolve(pkg.main);
+const compile = promisify(webpack);
 const args = process.argv.slice(2);
-const bump = args[0];
-const version = semver.inc(oldVersion, bump);
-if (version) {
-  pkg.version = version;
-  ShellString(pretty.format(pkg)).to("package.json");
-  exec("npm run build");
-  exec(`git add ${pkg.main} && git commit -m "${version}"`);
-  exec(`git tag -a v${version} -m "${version}"`);
-  exec(`git tag -af stable -m "${version}"`);
-  exec(`git push && git push --tags --force`);
+delete config.devtool;
+
+async function build(version = pkg.version) {
+  await compile(config);
+  const script = cat(path);
+  const head = header(version);
+  ShellString(`${head}\n${script}`).to(path);
 }
+
+(async function() {
+  try {
+    const version = pkg.version;
+    const bump = args[0];
+    const update = semver.inc(version, bump);
+    if (update) {
+      pkg.version = update;
+      ShellString(pretty.format(pkg)).to("package.json");
+      await build(update);
+      exec(`git add ${pkg.main} && git commit -m "${update}"`);
+      exec(`git tag -a v${update} -m "${update}"`);
+      exec(`git tag -af stable -m "${update}"`);
+      // exec(`git push && git push --tags --force`);
+    } else {
+      await build();
+      console.log("Compiled successfully.");
+    }
+  } catch (e) {
+    console.error(e);
+  }
+})();
--- us-header.js
+++ us-header.js
@@ -1,26 +1,29 @@
-const URL = process.env.npm_package_repository_url.replace(".git", "/raw/stable/" + process.env_npm_package_main);
-const VERSION = process.env.npm_package_version;
-const NAME = process.env.npm_package_config_name
-const AUTHOR = process.env.npm_package_author
-const NAMESPACE = process.env.npm_package_name
-const DESCRIPTION = process.env.npm_package_description;
-const YEAR = (new Date()).getUTCFullYear();
-module.exports = function (PRODUCTION, LOCAL) {
-  return `
-// ==UserScript==
+const pkg = require("./package.json");
+const URL = pkg.repository.url.replace(".git", "/raw/stable/" + pkg.main);
+const NAME = pkg.config.name;
+const AUTHOR = pkg.author;
+const NAMESPACE = pkg.name;
+const DESCRIPTION = pkg.description;
+const YEAR = new Date().getUTCFullYear();
+module.exports = function(VERSION = "DEVELOPMENT", LOCAL) {
+  const PRODUCTION = VERSION !== "DEVELOPMENT";
+  return `// ==UserScript==
 // @name         ${NAME}
 // @author       ${AUTHOR}
 // @namespace    ${NAMESPACE}
-// @version      ${PRODUCTION ? VERSION : "DEVELOPMENT"}
+// @version      ${VERSION}
 // @description  ${DESCRIPTION}
 // @match        https://www.fakku.net/*
 ${PRODUCTION ? `// @updateURL    ${URL}` : ""}
 ${PRODUCTION ? `// @downloadURL  ${URL}` : ""}
-${PRODUCTION ? "" : `// @require      ${LOCAL}`}
-// @grant        none
+${
+    PRODUCTION
+      ? "// @grant        none"
+      : `// @require      ${LOCAL}\n// @grant        none`
+  }
 // @run-at       document-end
 // ==/UserScript==
-/*
+/*!
   ${NAME} - ${DESCRIPTION}
   Copyright (C) ${YEAR} ${AUTHOR}
 
@@ -37,4 +40,4 @@ ${PRODUCTION ? "" : `// @require      ${LOCAL}`}
   You should have received a copy of the GNU Affero General Public License
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */`;
-}
+};
